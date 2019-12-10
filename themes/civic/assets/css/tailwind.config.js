584fe26a00700d88b5edba6456752365b3bb0444
milafrerichs <mila.frerichs@gmail.com>
2019-12-10 18:29:05 +0100

    feat: upate tailwindCSS to 1.3
    update tailwind library to 1.3 and update a little bit of content
    the landingpage is the biggest update to be considered

---
+++ themes/civic/assets/css/tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        civic: '#2b4c7e',
+      },
+      maxWidth: theme => {
+        return {
+          'screen-xl': theme('screens.xl'),
+        }
+      },
+    }
+  },
+  plugins: [
+    require("tailwind-heropatterns")({
+      variants: [],
+      patterns: ["topography", "signal"],
+      colors: {
+          default: "#fff",
+          "blue-light": "#adc5ea",
+          "blue-lighter": "#abc7f1"
+        },
+      opacity: {
+          default: "0.4",
+          "20": "0.2",
+          "10": "0.1"
+        }
+    }),
+  ],
+};
