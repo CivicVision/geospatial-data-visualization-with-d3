793bd3e682dc52b287d24fa140982698a128fad7
Mila Frerichs <mila.frerichs@gmail.com>
2020-02-02 09:13:30 +0100

    feat: add trackiong to Convertkit which posts you read
    add a tag for the posts subscribers read, and only subsribers.
    depending on what they already read they don't get certain emails

---
+++ static/javascript/ck.js
@@ -0,0 +1,25 @@
+window.sendTags = function(subscriberId, tagId) {
+
+  const url = 'https://baihh9wyp5.execute-api.us-east-1.amazonaws.com/dev/tag_subscriber';
+  const options = {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json;charset=UTF-8'
+    },
+    body: JSON.stringify({
+      subscriber_id: subscriberId,
+      tag_id: tagId
+    })
+  };
+  fetch(url, options)
+    .then(response => {
+      console.log(response.status);
+    });
+}
+window.maybeAddTags = function(post) {
+	var ckId = localStorage.getItem('rmpanda.sources.convertkit.subscriberId');
+  if(ckId != "" && post.tagId != "") {
+    window.sendTags(ckId, post.tagId);
+  }
+}
