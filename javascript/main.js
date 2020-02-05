71bb4c96027c78c34b958d01aeba798aa038bd14
mila.frerichs@gmail.com <mila.frerichs@gmail.com>
2020-02-05 11:52:55 +0000

    Deploy from Forestry.io

---
+++ javascript/main.js
@@ -0,0 +1,24 @@
+window.addLoadEvent = function(func) {
+  var oldonload = window.onload;
+  if (typeof window.onload != 'function') {
+    window.onload = func;
+  } else {
+    window.onload = function() {
+      if (oldonload) {
+        oldonload();
+      }
+      func();
+    };
+  }
+};
+function menu() {
+  let menu = document.getElementById('menu')
+  let menuContainer = document.getElementById('menu-container')
+  let menuHeader = document.getElementById('menu-header')
+  menu.onclick = function(e) {
+    menuContainer.classList.toggle('hidden');
+    menuHeader.classList.toggle('fixed');
+    menuHeader.classList.toggle('top-0');
+  };
+}
+addLoadEvent(menu);
